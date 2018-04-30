ad135db434bf0ea8dd4a537d2d32ebee3ea34935
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-04-29 21:57:13 -0500

    linting
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 #
 #  Original solution via StackOverflow:
 #    http://stackoverflow.com/questions/35802939/install-only-available-packages-using-conda-install-yes-file-requirements-t
 #
 
-while read requirement; do conda install --yes $requirement; done < ./requirements-conda.txt
+while read requirement; do
+    conda install --yes $requirement;
+done < ./requirements-conda.txt
