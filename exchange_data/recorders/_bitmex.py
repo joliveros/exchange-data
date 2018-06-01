40a2cc542fc7a690b0e606faafd08d5b2501bfcf
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-05-31 19:33:08 -0500

    update bitmex-websocket, cli to use builtin runners instead of asyncio
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/recorders/_bitmex.py
+++ exchange_data/recorders/_bitmex.py
@@ -1,3 +1,5 @@
+from bitmex_websocket.constants import InstrumentChannels
+
 from .. import settings
 from . import Recorder
 from bitmex_websocket import Instrument
@@ -7,40 +9,34 @@ import websocket
 alog.set_level(settings.LOG_LEVEL)
 
 
-class BitmexRecorder(Recorder):
+class BitmexRecorder(Recorder, Instrument):
     measurements = []
     channels = [
-        'quote',
-        'trade',
-        'orderBookL2'
+        InstrumentChannels.quote,
+        InstrumentChannels.trade,
+        InstrumentChannels.orderBookL2
     ]
 
-    def __init__(self, symbols):
-        super().__init__(symbols, database_name='bitmex')
+    def __init__(self, symbol, database_name='bitmex'):
+        super().__init__(symbol, database_name)
         websocket.enableTrace(settings.RUN_ENV == 'development')
 
-        self.symbols = symbols
+        self.symbol = symbol
+        Instrument.__init__(self, symbol=symbol,
+                            channels=self.channels,
+                            should_auth=False)
 
-        for symbol in self.symbols:
-            self.subscribe_symbol(symbol)
-
-    def subscribe_symbol(self, symbol):
-        instrument = Instrument(symbol=symbol,
-                                channels=self.channels,
-                                # set to 1 because data will be saved to db
-                                shouldAuth=False)
-
-        instrument.on('latency',
-                      lambda latency: alog.debug(
-                          "latency: {0}".format(latency)))
-        instrument.on('action', self.on_action)
+        self.on('action', self.on_action)
 
     def on_action(self, data):
-        data = self.to_lowercase_keys(data)
-        data['symbol'] = data['data'][0]['symbol']
+        data['symbol'] = self.symbol
         data['timestamp'] = self.get_timestamp()
 
         for row in data['data']:
             row.pop('symbol', None)
 
-        self.save_measurement('data', data['symbol'], data)
+        alog.debug(alog.pformat(data))
+        self.save_measurement('data', self.symbol, data)
+
+    def start(self, **kwargs):
+        self.run_forever(**kwargs)
