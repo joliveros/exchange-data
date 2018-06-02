9e36cce363c078eb6bd7a75c2dfb4a8ea66dec5a
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-06-02 17:13:53 -0500

    upgrade to latest bitmex-websocket
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/settings.py
+++ exchange_data/settings.py
@@ -1,14 +1,13 @@
 import logging
 
 import rollbar
-from dotenv import load_dotenv, find_dotenv
 from os import environ
 import alog
 
-load_dotenv(find_dotenv(), override=True)
-
 RUN_ENV = environ.get('RUN_ENV')
+
 DB = environ.get('DB')
+
 BITSTAMP_PUSHER_APP_KEY = environ.get('BITSTAMP_PUSHER_APP_KEY')
 
 if RUN_ENV != 'development':
@@ -29,7 +28,6 @@ else:
 alog.debug('batch size: %s' % MEASUREMENT_BATCH_SIZE)
 
 
-INFLUX_DB = environ.get('INFLUX_DB')
 LOG_LEVEL = environ.get('LOG_LEVEL')
 
 if LOG_LEVEL is None:
@@ -40,7 +38,5 @@ LOG_LEVEL = logging.getLevelName(LOG_LEVEL)
 alog.debug(LOG_LEVEL)
 
 alog.set_level(LOG_LEVEL)
-
-alog.debug(INFLUX_DB)
 
 CERT_FILE = './ca.pem'
