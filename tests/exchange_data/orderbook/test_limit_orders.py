fd2bb204210ccc1533fe5ea66cc1fc1256339e3e
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-06-03 17:06:22 -0500

    test limit orders
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tests/exchange_data/orderbook/test_limit_orders.py
+++ tests/exchange_data/orderbook/test_limit_orders.py
@@ -1,119 +1,68 @@
 import alog
-import pytest
 
-from exchange_data.orderbook import OrderBook, BuyOrder, SellOrder
-from exchange_data.orderbook._order import OrderType
-from exchange_data.orderbook._transcation import Transaction
-
-
-@pytest.fixture
-def orders():
-    return [
-        SellOrder(
-            price=120.00,
-            quantity=5
-        ),
-        SellOrder(
-            price=110.00,
-            quantity=5
-        ),
-        SellOrder(
-            price=100.00,
-            quantity=5
-        ),
-        BuyOrder(
-            price=90.00,
-            quantity=5
-        ),
-        BuyOrder(
-            price=80.00,
-            quantity=5
-        ),
-        BuyOrder(
-            price=70.00,
-            quantity=5
-        )
-    ]
+from .fixtures import orderbook, orders
+from exchange_data.orderbook import OrderBook, BuyOrder, SellOrder, \
+    OrderType, OrderList, Transaction
 
 
-@pytest.fixture
-def orderbook(orders):
-    _orderbook = OrderBook()
-
-    for order in orders:
-        _orderbook.process_order(order)
-
-    return _orderbook
-
-
-class TestOrderBook(object):
+class TestLimitOrders(object):
     def test_process_limit_order(self):
-        order = SellOrder(
-            order_type=OrderType.LIMIT,
-            price=101.00,
-            quantity=5
-        )
+        order = SellOrder(order_type=OrderType.LIMIT, price=101.00, quantity=5)
 
         order_book = OrderBook()
 
         order_book.process_order(order)
 
-    def test_crossing_limit_order(self, orderbook: OrderBook):
+    def test_limit_order_cross_best_ask(self, orderbook: OrderBook):
         crossing_limit_order = BuyOrder(101.0, 2)
-
         trades, order = orderbook.process_order(crossing_limit_order)
 
         assert order is None
-
         assert len(trades) == 1
+
         trade: Transaction = trades[0]
 
-        # Ensure that the corresponding price level does not exist
-        with pytest.raises(KeyError):
-            orderbook.asks.get_price_list(crossing_limit_order.price)
+        min_price_list: OrderList = orderbook.asks.min_price_list()
 
-        # alog.debug(trade)
+        assert min_price_list.volume == 3
+        assert trade.party1.counter_party_id == min_price_list.head_order.uid
 
-        order_list = orderbook.asks.price_map[100]
+    def test_limit_order_cross_two_levels_into_ask(self, orderbook: OrderBook):
+        crossing_limit_order = BuyOrder(110.0, 10)
+        trades, order = orderbook.process_order(crossing_limit_order)
 
-        assert order_list.volume == 3
+        assert order is None
+        assert len(trades) == 2
 
-        # assert trade.party1.counter_party_id == orderbook.asks.get_price_list()
+        min_price_list: OrderList = orderbook.asks.min_price_list()
 
-# # Add orders to order book
-# for order in limit_orders:
-#     trades, order_id = order_book.process_order(order, False, False)
-#
-# # The current book may be viewed using a print
-# print(order_book)
-#
-# # Submitting a limit order that crosses the opposing best price will result in a trade
-# crossing_limit_order = {'type': 'limit',
-#                         'side': 'bid',
-#                         'quantity': 2,
-#                         'price': 102,
-#                         'trade_id': 109}
-#
-# print(crossing_limit_order)
-# trades, order_in_book = order_book.process_order(crossing_limit_order, False, False)
-# print("Trade occurs as incoming bid limit crosses best ask")
-# print(trades)
-# print(order_book)
-#
-# # If a limit crosses but is only partially matched, the remaning volume will
-# # be placed in the book as an outstanding order
-# big_crossing_limit_order = {'type': 'limit',
-#                             'side': 'bid',
-#                             'quantity': 50,
-#                             'price': 102,
-#                             'trade_id': 110}
-# print(big_crossing_limit_order)
-# trades, order_in_book = order_book.process_order(big_crossing_limit_order, False, False)
-# print("Large incoming bid limit crosses best ask. Remaining volume is placed in book.")
-# print(trades)
-# print(order_book)
-#
-#
+        assert min_price_list.head_order.uid == 0
+
+    def test_limit_order_cross_best_bid(self, orderbook: OrderBook):
+        crossing_limit_order = SellOrder(89, 2)
+        trades, order = orderbook.process_order(crossing_limit_order)
+
+        assert order is None
+        assert len(trades) == 1
+
+        trade: Transaction = trades[0]
+
+        max_price_list: OrderList = orderbook.bids.max_price_list()
+
+        assert max_price_list.volume == 3
+        assert trade.party1.counter_party_id == max_price_list.head_order.uid
+
+    def test_limit_order_cross_two_levels_into_bid(self, orderbook: OrderBook):
+        crossing_limit_order = SellOrder(80, 10)
+        trades, order = orderbook.process_order(crossing_limit_order)
+
+        assert order is None
+        assert len(trades) == 2
+
+        max_price_list: OrderList = orderbook.bids.max_price_list()
+
+        assert max_price_list.head_order.uid == 5
+
 # # Market Orders
 #
 # # Market orders only require that a user specifies a side (bid or ask), a quantity, and their unique trade id
--- tests/exchange_data/tf_orderbook/fixtures.py
+++ tests/exchange_data/tf_orderbook/fixtures.py
@@ -7,5 +7,4 @@ def datafile_name(name):
     path = f'../data/{name}.json'
     dir = os.path.join(os.path.dirname(__file__))
     path = os.path.join(dir, path)
-    alog.debug(path)
     return path
