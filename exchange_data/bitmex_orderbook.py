3648df656150db54d4eca70d6637177a07b4965c
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-06-17 00:03:24 -0500

    temp
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/bitmex_orderbook.py
+++ exchange_data/bitmex_orderbook.py
@@ -1,11 +1,13 @@
-import json
 from enum import auto
 from typing import Any
-
 import alog
+import json
+import sys
+import time
+import traceback
 
-from exchange_data.hdf5_orderbook import Hdf5OrderBook
-from exchange_data.orderbook import Order, NoValue, OrderBookSide, OrderType
+from exchange_data.orderbook import Order, NoValue, OrderBookSide, OrderType, \
+    OrderBook
 from exchange_data.orderbook.exceptions import OrderExistsException, \
     PriceDoesNotExistException
 
@@ -13,6 +15,7 @@ from exchange_data.orderbook.exceptions import OrderExistsException, \
 class ActionType(NoValue):
     INSERT = auto()
     UPDATE = auto()
+    PARTIAL = auto()
     DELETE = auto()
 
 
@@ -36,12 +39,22 @@ class BitmexOrder(Order):
 
 
 class Action(object):
-    def __init__(self, symbol: str, data: str):
+    def __init__(self, symbol: str, data: str, timestamp: int=None):
         self.symbol = symbol
-        data = json.loads(data)
+
+        if type(data) == str:
+            data = json.loads(data)
+
         self.type = ActionType[data['action'].upper()]
         self.table = data['table']
-        self.timestamp = data['timestamp']
+
+        if 'timestamp' in data:
+            self.timestamp = data['timestamp']
+        elif timestamp:
+            self.timestamp = timestamp
+        else:
+            self.timestamp = time.time()
+
         self.orders = data['data']
 
 
@@ -51,17 +64,23 @@ class BitmexMessage(object):
             data = json.loads(data)
         self.data: Action = None
 
-        self.symbol: str = data['symbol']
+        if 'symbol' in data:
+            self.symbol: str = data['symbol']
+        else:
+            self.symbol = data['data'][0]['symbol']
 
-        self.timestamp: int = data['time']
+        if 'time' in data:
+            self.timestamp: int = data['time']
+        else:
+            self.timestamp = time.time()
 
-        self.action = Action(self.symbol, data['data'])
+        self.action = Action(self.symbol, data, self.timestamp)
 
     def __str__(self):
         return str(self.__dict__)
 
 
-class BitmexOrderBook(Hdf5OrderBook):
+class BitmexOrderBook(OrderBook):
     def __init__(self,
                  symbol: str,
                  cache_dir: str = None,
@@ -70,27 +89,28 @@ class BitmexOrderBook(Hdf5OrderBook):
                  read_from_json: bool = False,
                  total_time='1d'
                  ):
-        super().__init__(
-            cache_dir=cache_dir,
-            database='bitmex',
-            file_check=file_check,
-            overwrite=overwrite,
-            read_from_json=read_from_json,
-            symbol=symbol,
-            total_time=total_time
-        )
+        super().__init__()
 
-    def on_message(self, raw_message):
-        message = BitmexMessage(raw_message)
+    def message(self, raw_message):
+        try:
+            message = BitmexMessage(raw_message)
 
-        if message.action.table == 'orderBookL2':
-            self.order_book_l2(message)
+            if message.action.table == 'orderBookL2':
+                self.order_book_l2(message)
+
+            print(self)
+
+        except Exception:
+            traceback.print_exc()
 
     def order_book_l2(self, message):
+        alog.debug(message)
+        # sys.exit()
+
         if message.action.type == ActionType.UPDATE:
             self.update_orders(message)
 
-        elif message.action.type == ActionType.INSERT:
+        elif message.action.type in [ActionType.INSERT, ActionType.PARTIAL]:
             orders = [BitmexOrder(order_data, message.timestamp)
                       for order_data in message.action.orders]
 
@@ -114,7 +134,7 @@ class BitmexOrderBook(Hdf5OrderBook):
 
         for line in self.result_set['data']:
             try:
-                self.on_message(line)
+                self.message(line)
                 print(self)
                 self.relative_orderbook()
 
@@ -124,5 +144,9 @@ class BitmexOrderBook(Hdf5OrderBook):
     def relative_orderbook(self):
         if self.bids is not None and len(self.bids) > 0:
             alog.debug(self.bids.price_tree.max_key())
+
         if self.asks is not None and len(self.asks) > 0:
             alog.debug(self.asks.price_tree.min_key())
+
+    def fetch_measurements(self):
+        raise NotImplementedError()
