e174a880c3bde3775c49efa33c07a49fc5939ef0
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-07-08 21:49:34 -0500

    temp
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tests/exchange_data/test_bitmex_orderbook_gym_data.py
+++ tests/exchange_data/test_bitmex_orderbook_gym_data.py
@@ -1,14 +1,30 @@
 import alog
 
 from exchange_data.bitmex_orderbook_gym_data import BitmexOrderBookGymData
+from tests.exchange_data.fixtures import instruments
 
 
 class TestBitmexOrderBookGymData(object):
 
-    def test_fetch_and_save(self, mocker, tmpdir):
+    def test_fetch_and_save(self, mocker, tmpdir, instruments):
+        mocker.patch.object(BitmexOrderBookGymData, '_instrument_data',
+                            lambda context: instruments)
+
         book = BitmexOrderBookGymData(symbol='XBTUSD',
                                       total_time='15m', overwrite=False,
                                       cache_dir=tmpdir,
                                       read_from_json=True)
+
+        book.fetch_and_save()
+
+    def test_fetch_and_save_30_second_interval(self, mocker, tmpdir,
+                                               instruments):
+        mocker.patch.object(BitmexOrderBookGymData, '_instrument_data',
+                            lambda context: instruments)
+        book = BitmexOrderBookGymData(symbol='XBTUSD',
+                                      total_time='15m', overwrite=False,
+                                      cache_dir=tmpdir,
+                                      read_from_json=True,
+                                      interval='15s')
 
         book.fetch_and_save()
