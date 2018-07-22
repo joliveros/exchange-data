753ab5ba09a53e44ac52136944ff6f9110cb821d
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-07-22 12:28:51 -0500

    add date_plus_timestring()
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/utils.py
+++ exchange_data/utils.py
@@ -1,5 +1,12 @@
 import math
+from datetime import datetime
+from pytimeparse import parse as dateparse
 
 
 def roundup_to_nearest(value, interval=10.0):
     return math.ceil(value / interval) * interval
+
+
+def date_plus_timestring(timestamp: int, total_time: str) -> datetime:
+    total_time_ms = dateparse(total_time) * 1000
+    return datetime.fromtimestamp((total_time_ms + timestamp) / 1000)
