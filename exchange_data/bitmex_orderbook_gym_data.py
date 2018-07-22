aac9fdac4c7355e05d2505452d8fa8b5649d72be
Jose Oliveros <jose.oliveros.1983@gmail.com>
2018-07-22 14:39:06 -0500

    temp
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/bitmex_orderbook_gym_data.py
+++ exchange_data/bitmex_orderbook_gym_data.py
@@ -1,43 +1,102 @@
-import time
-
 from exchange_data.bitmex_orderbook import BitmexOrderBook
-from exchange_data.hdf5_orderbook import Hdf5OrderBook
-from pytimeparse import parse as dateparse
-
+from exchange_data.cached_dataset import CachedDataset
+from exchange_data.influxdb_data import InfluxDBData
+from exchange_data.utils import date_plus_timestring, datetime_from_timestamp
+from pandas import date_range
 import alog
-import datetime
 import numpy as np
 
 
-class BitmexOrderBookGymData(BitmexOrderBook, Hdf5OrderBook):
+class BitmexOrderBookGymData(BitmexOrderBook, CachedDataset, InfluxDBData):
     def __init__(self,
                  symbol: str,
                  overwrite: bool,
                  cache_dir: str = None,
                  total_time: str = '15m',
-                 read_from_json: bool = False
+                 read_from_json: bool = False,
+                 max_levels=1000
                  ):
-        database = 'bitmex'
-        Hdf5OrderBook.__init__(self,
-                               database=database,
-                               symbol=symbol,
-                               overwrite=overwrite,
-                               cache_dir=cache_dir,
-                               read_from_json=read_from_json,
-                               total_time=total_time)
+        self.freq = '1s'
+
+        self.max_levels = max_levels
+
+        self.date_range = None
+
+        self.total_time = total_time
+
+        self.database = 'bitmex'
+
+        self.current_time = None
+
+        self._last_index = 0
+
+        CachedDataset.__init__(
+            self,
+            overwrite=overwrite,
+            cache_dir=cache_dir
+        )
 
         BitmexOrderBook.__init__(self, symbol=symbol)
 
-        self.current_time = None
-        self._last_index = 0
+        InfluxDBData.__init__(
+            self,
+            database=self.database,
+            read_from_json=read_from_json,
+            symbol=symbol,
+            total_time=total_time
+        )
 
     @property
     def dataset_name(self):
         return f'{self.database}_{self.symbol}_{self.interval}'
 
     @property
+    def filename(self):
+        return f'{self.cache_dir}/{self.storage_name}_{self.total_time}.' \
+               f'{self.extension} '
+
+    @property
     def half_spread(self):
         return self.spread / 2
+
+    @property
+    def bid_volume_by_percent(self):
+        volume_pct = np.true_divide(self.bid_volume, np.sum(self.bid_volume))
+        return volume_pct
+
+    @property
+    def bid_volume(self):
+        volume = np.array(
+            [level[1].volume for level in self.sorted_bid_levels]
+        )
+        return volume
+
+    @property
+    def sorted_bid_levels(self):
+        return list(self.bids.price_tree.items(reverse=True))
+
+    def nearest_date(self):
+        last_datetime = datetime_from_timestamp(self.last_timestamp)
+        # nearest_date = min(self.dataset.time)
+
+        # round to nearest freq
+
+
+    def init_dataset(self):
+        time_index = date_range(
+            start=self.date_range['start'],
+            end=self.date_range['end'],
+            freq=self.freq)
+        self.dataset['time'] = time_index
+
+        alog.debug(time_index)
+
+        volume_pct_shape = (time_index.shape[0], self.max_levels)
+        volume_pct_data = np.zeros(volume_pct_shape)
+        self.dataset['bid_volume_pct'] = \
+            (['time', 'volume_pct'], volume_pct_data)
+
+        # alog.debug(self.dataset)
 
     def fetch_and_save(self):
         self.fetch_measurements()
@@ -45,35 +104,38 @@ class BitmexOrderBookGymData(BitmexOrderBook, Hdf5OrderBook):
         for line in self.result_set['data']:
             self.replay(line)
 
-        self.file.close()
+        self.save()
 
     def replay(self, line):
-        msg = self.message(line)
+        msg = self.message_strict(line)
+
+        if self.date_range is None:
+            self.read_date_range(msg)
+            self.init_dataset()
+
         self.save_frame()
 
     def save_frame(self):
-        orderbook_frame = self.gen_orderbook_frame()
-        self.storage.append(orderbook_frame)
+        pass
+        # alog.debug(self.bid_volume_by_percent)
+        # alog.debug(self.dataset.volume_pct['time'][self.last_timestamp])
 
-    def gen_orderbook_frame(self):
-        bid_side = self.gen_bid_side()
-        bid_side.resize((2000, 2))
-        bid_side -= self.half_spread
+        # bid_side.resize((2000, 2))
+        # bid_side -= self.half_spread
+        #
+        # ask_side = self.gen_ask_side()
+        # ask_side.resize((2000, 2))
+        # ask_side += self.half_spread
 
-        ask_side = self.gen_ask_side()
-        ask_side.resize((2000, 2))
-        ask_side += self.half_spread
-        frame = np.array([ask_side, bid_side])
-        alog.debug(frame)
-        return frame
+        # frame = np.array([ask_side, bid_side])
+
+        # alog.debug(frame)
+
+        # return frame
 
     def gen_bid_side(self):
-        bid_levels = list(self.bids.price_tree.items(reverse=True))
         price = np.array(
-            [level[0] for level in bid_levels]
-        )
-        volume = np.array(
-            [level[1].volume for level in bid_levels]
+            [level[0] for level in self.sorted_bid_levels]
         )
 
         if len(price) == 0:
@@ -81,9 +143,7 @@ class BitmexOrderBookGymData(BitmexOrderBook, Hdf5OrderBook):
         else:
             max_bid_diff = price - price[0]
 
-        volume_pct = np.true_divide(volume, np.sum(volume))
-
-        return np.vstack((max_bid_diff, volume_pct))
+        return max_bid_diff
 
     def gen_ask_side(self):
         ask_levels = list(self.asks.price_tree.items())
@@ -104,16 +164,11 @@ class BitmexOrderBookGymData(BitmexOrderBook, Hdf5OrderBook):
 
         return np.vstack((max_ask_diff, volume_pct))
 
-    def dataset(self, file):
-        if self._dataset:
-            return self._dataset
-        elif self.dataset_name in file:
-            return file[self.dataset_name]
-        else:
-            return file.create_dataset(self.dataset_name, (0, 0, 0, 0),
-                                       dtype=float, chunks=True,
-                                       maxshape=(float('1e5'), float('1e5'),
-                                                 float('1e5'), float('1e5'),))
+    def read_date_range(self, msg):
+        self.date_range = {
+            'start': msg.timestamp_datetime,
+            'end': date_plus_timestring(msg.timestamp, self.total_time)
+        }
 
 
 if __name__ == '__main__':
--- exchange_data/utils.py
+++ exchange_data/utils.py
@@ -9,4 +9,8 @@ def roundup_to_nearest(value, interval=10.0):
 
 def date_plus_timestring(timestamp: int, total_time: str) -> datetime:
     total_time_ms = dateparse(total_time) * 1000
+    return datetime_from_timestamp(timestamp, total_time_ms)
+
+
+def datetime_from_timestamp(timestamp, total_time_ms):
     return datetime.fromtimestamp((total_time_ms + timestamp) / 1000)
