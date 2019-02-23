0bba62abd0b911ccf6226664dfbb9a2a658964db
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-02-23 14:12:57 -0600

    remove unused types
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/child.py
+++
@@ -1,7 +0,0 @@
-class Child(object):
-    def __init__(self, *args, **kwds):
-        if len(args) == 1 and str(type(args[0])) == "<class '__main__.Parent'>":
-            new_args = [args[0].x, args[0].y, args[0].z]
-            super(Child, self).__init__(*new_args, **kwds)
-        else:
-            super(Child, self).__init__(*args, **kwds)
