d57d948c28528d9c93b921f252928debc270735f
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-03-06 15:23:17 -0600

    add signal interceptor
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/emitters/__init__.py
+++ exchange_data/emitters/__init__.py
@@ -1,5 +1,11 @@
 from .messenger import Messenger
 from .time_emitter import TimeEmitter, TimeChannels
+from typing import Callable
+
+import signal
 
 
-
+class SignalInterceptor(object):
+    def __init__(self, exit_func: Callable):
+        signal.signal(signal.SIGINT, exit_func)
+        signal.signal(signal.SIGTERM, exit_func)
