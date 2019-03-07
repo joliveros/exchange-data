8289843acfadc7799d892a5a2e1ffbbc9b53b7ff
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-03-06 19:32:36 -0600

    use vcr for tests
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tgym/envs/orderbook/tests/test_orderbook_trading.py
+++ tgym/envs/orderbook/tests/test_orderbook_trading.py
@@ -1,11 +1,15 @@
+import pytest
+
 from dateutil import parser, tz
 from tgym.envs import OrderBookTradingEnv
 from tgym.envs.orderbook import Actions
 
 
 class TestOrderBookTradingEnv(object):
+
+    @pytest.mark.vcr(record_mode='all')
     def test_init(self):
-        start_date = parser.parse('2019-03-06 21:49:07.807158+00:00') \
+        start_date = parser.parse('2019-03-07 01:31:48.315491+00:00') \
             .replace(tzinfo=tz.tzutc())
 
         env = OrderBookTradingEnv(
