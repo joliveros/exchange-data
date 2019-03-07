fa9a454d56bd79d5e8305335f97090974ed7a228
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-03-07 01:13:39 -0600

    orderbook env produces obs with orderbook, and position state
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tgym/envs/orderbook/__init__.py
+++ tgym/envs/orderbook/__init__.py
@@ -1,25 +1,15 @@
-import numpy as np
+from exchange_data.utils import NoValue
 
 
-class Actions(list):
-    Buy = np.array([0, 1, 0])
-    Hold = np.array([1, 0, 0])
-    Sell = np.array([0, 1, 0])
-
-    def __init__(self):
-        super().__init__()
-        self.append(self.Buy)
-        self.append(self.Hold)
-        self.append(self.Sell)
+class Actions(NoValue):
+    Buy = 0
+    Hold = 1
+    Sell = 2
 
 
-class Positions(list):
-    Flat = np.array([1, 0, 0])
-    Long = np.array([0, 1, 0])
-    Short = np.array([0, 1, 0])
+class Positions(NoValue):
+    Flat = 0
+    Long = 1
+    Short = 2
 
-    def __init__(self):
-        super().__init__()
-        self.append(self.Flat)
-        self.append(self.Long)
-        self.append(self.Short)
+
--- tgym/envs/orderbook/tests/test_orderbook_trading.py
+++ tgym/envs/orderbook/tests/test_orderbook_trading.py
@@ -1,14 +1,19 @@
+from time import sleep
+
+import alog
 import pytest
 
 from dateutil import parser, tz
+from pytimeparse import timeparse
+
 from tgym.envs import OrderBookTradingEnv
-from tgym.envs.orderbook import Actions
+from tgym.envs.orderbook import Actions, Positions
 
 
 class TestOrderBookTradingEnv(object):
 
-    @pytest.mark.vcr(record_mode='all')
-    def test_init(self):
+    @pytest.mark.vcr()
+    def test_orderbook_env_reset(self):
         start_date = parser.parse('2019-03-07 01:31:48.315491+00:00') \
             .replace(tzinfo=tz.tzutc())
 
@@ -17,4 +22,28 @@ class TestOrderBookTradingEnv(object):
             start_date=start_date
         )
 
-        env.step(Actions.Buy)
+        env.total_reward += 10
+
+        env.reset()
+
+        assert env.total_reward == 0
+
+    # @pytest.mark.vcr(record_mode='once')
+    def test_orderbook_env_step(self):
+        start_date = parser.parse('2019-03-07 01:31:48.315491+00:00') \
+            .replace(tzinfo=tz.tzutc())
+
+        env = OrderBookTradingEnv(
+            window_size='1s',
+            random_start_date=True
+            # start_date=start_date
+        )
+
+        for i in range(timeparse.timeparse('35m')):
+            env.step(Positions.Long.value)
+            sleep(0.2)
+
+        env.step(Positions.Flat.value)
+
+        # alog.info(env.frames)
+
