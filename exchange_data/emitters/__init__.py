eb8182d5900fe02396e3e90fc276e065529daf9a
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-03-08 15:40:25 -0600

    allow streamer to continue upon finding empty results
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/emitters/__init__.py
+++ exchange_data/emitters/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from .messenger import Messenger
 from .time_emitter import TimeEmitter, TimeChannels
 from typing import Callable
@@ -6,6 +8,11 @@ import signal
 
 
 class SignalInterceptor(object):
-    def __init__(self, exit_func: Callable):
+    def __init__(self, exit_func: Callable = None):
+        if exit_func is None:
+            exit_func = self.exit
         signal.signal(signal.SIGINT, exit_func)
         signal.signal(signal.SIGTERM, exit_func)
+
+    def exit(self, *args):
+        sys.exit(0)
