7ef1ae4224edfa4a508080420f033f3b7d959f1c
Jose Oliveros <jose.oliveros.1983@gmail.com>
2019-08-10 16:04:13 -0500

    restructure models
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/models/resnet/model.py
+++ exchange_data/models/resnet/model.py
@@ -1,8 +1,6 @@
-import alog
-from tensorflow.python.training.session_run_hook import SessionRunHook, SessionRunArgs
 from tensorflow_estimator.python.estimator.run_config import RunConfig
-
 from exchange_data import settings
+from exchange_data.models.pnl_hook import ProfitAndLossHook
 from exchange_data.tfrecord.dataset import dataset
 from pathlib import Path
 from pytimeparse.timeparse import timeparse
@@ -15,38 +13,14 @@ from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
 from tensorflow_estimator.python.estimator.training import TrainSpec, EvalSpec, train_and_evaluate
 import shutil
 import click
-import tensorflow
+import tensorflow as tf
 
 
-class ProfitAndLossHook(SessionRunHook):
-    def __init__(self):
-        super(SessionRunHook).__init__()
-
-    def begin(self):
-        pass
-
-    def before_run(self, run_context):
-        return SessionRunArgs()
-
-    def after_run(self, run_context, run_values):
-        loss_value = run_values.results
-        print("loss value:", loss_value)
-
-
-@click.command()
-@click.option('--epochs', '-e', type=int, default=10)
-@click.option('--batch-size', '-b', type=int, default=20)
-@click.option('--learning-rate', '-l', type=float, default=0.1)
-@click.option('--clear', '-c', is_flag=True)
-def main(epochs, batch_size, clear, learning_rate, **kwargs):
-    tensorflow.compat.v1.logging.set_verbosity(settings.LOG_LEVEL)
-
+def Model(learning_rate):
     model = models.Sequential()
     base = ResNet50(include_top=False, weights=None, classes=3)
-
     for layer in base.layers:
         layer.trainable = True
-
     model.add(Input(shape=(115, 115, 3)))
     model.add(base)
     model.add(GlobalAveragePooling2D())
@@ -58,6 +32,19 @@ def main(epochs, batch_size, clear, learning_rate, **kwargs):
     model.compile(loss='sparse_categorical_crossentropy',
                   optimizer=SGD(lr=learning_rate, decay=5e-3),
                   metrics=['accuracy'])
+    return model
+
+
+@click.command()
+@click.option('--epochs', '-e', type=int, default=10)
+@click.option('--batch-size', '-b', type=int, default=20)
+@click.option('--learning-rate', '-l', type=float, default=0.3e-4)
+@click.option('--clear', '-c', is_flag=True)
+@click.option('--eval-span', type=str, default='20m')
+def main(epochs, batch_size, clear, learning_rate, eval_span, **kwargs):
+    tf.compat.v1.logging.set_verbosity(settings.LOG_LEVEL)
+
+    model = Model(learning_rate)
 
     model.summary()
 
@@ -70,22 +57,22 @@ def main(epochs, batch_size, clear, learning_rate, **kwargs):
             pass
 
     run_config = RunConfig(save_checkpoints_steps=100)
-    est_resnet = model_to_estimator(
+    resnet_estimator = model_to_estimator(
         keras_model=model, model_dir=model_dir,
         checkpoint_format='checkpoint',
         config=run_config,
     )
-    # alog.info(alog.pformat(est_resnet.get_variable_names()))
-    eval_span = timeparse('20m')
+    resnet_estimator.predict()
+    eval_span = timeparse(eval_span)
     train_spec = TrainSpec(input_fn=lambda: dataset(batch_size, epochs).skip(eval_span),
                            max_steps=epochs * 6 * 60 * 60)
     eval_spec = EvalSpec(
         input_fn=lambda: dataset(batch_size, 1).take(eval_span),
         steps=eval_span,
-        hooks=[ProfitAndLossHook()]
+        hooks=[ProfitAndLossHook(resnet_estimator)]
     )
 
-    train_and_evaluate(est_resnet, train_spec, eval_spec)
+    train_and_evaluate(resnet_estimator, train_spec, eval_spec)
 
 
 if __name__ == '__main__':
