affab3344eb975c647fece1827ccca9ff4ee3c6d
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-01-04 23:59:16 -0600

    update dataset
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/tfrecord/dataset.py
+++ exchange_data/tfrecord/dataset.py
@@ -1,70 +1,53 @@
 #!/usr/bin/env python
 
-import sys
-
 import tensorflow as tf
+
 from pathlib import Path
 
+import alog
 import click
 
 
+FixedLenFeature = tf.io.FixedLenFeature
+
 CLASSES = [0, 1, 2]
-# table = index_table_from_tensor(tf.constant(CLASSES), dtype=int64)
 NUM_CLASSES = len(CLASSES)
 
 
 def extract_fn(data_record):
-    features = {
-        'datetime': tf.io.FixedLenFeature([], tf.string),
-        'frame': tf.io.FixedLenFeature([224, 224, 3], tf.float32),
-        # 'diff': FixedLenFeature([1], tf.float32),
-        'expected_position': tf.io.FixedLenFeature([1,], tf.int64),
-    }
+    features = dict(
+        best_ask=FixedLenFeature([1], tf.float32),
+        best_bid=FixedLenFeature([1], tf.float32),
+        datetime=FixedLenFeature([], tf.string),
+        expected_position=tf.io.FixedLenFeature([1, ], tf.int64),
+        frame=FixedLenFeature([224, 224, 3], tf.float32),
+    )
 
-    sample = tf.io.parse_single_example(data_record, features)
-    img = sample['frame']
-
-    label = sample['expected_position']
-
-    # label = tf.one_hot(label, NUM_CLASSES, dtype=tf.dtypes.int64)
-    # return img, tf.argmax(label, axis=0)
-
-    return img, label
+    return tf.io.parse_single_example(data_record, features)
 
 
 def dataset(batch_size: int, epochs: int = 1, dataset_name='default'):
     # raise Exception()
     records_dir = f'{Path.home()}/.exchange-data/tfrecords/{dataset_name}'
     files = [str(file) for file in Path(records_dir).glob('*.tfrecord')]
+    num_files = len(files)
     files = tf.compat.v2.data.Dataset.from_tensor_slices(files)
-    # files = DatasetV2.from_tensor_slices(files)
 
     _dataset = files.flat_map(
         lambda filename: (tf.data.TFRecordDataset(filename, compression_type='GZIP'))
-    )
+    ).shuffle(buffer_size=num_files)
 
     _dataset = _dataset.map(extract_fn)\
         .batch(batch_size) \
         .repeat(epochs)
 
-    # _dataset = _dataset.map(extract_fn).window(6, drop_remainder=True)\
-    #     .flat_map(lambda x, y: Dataset.zip((x.batch(6), y.skip(5).batch(1))))\
-    #     .batch(2) \
-    #     .repeat(epochs)
-
     return _dataset
-    # for r in _dataset.take(10):
-    #     alog.info(r)
 
 
 @click.command()
-# @click.option('--summary-interval', '-s', default=6, type=int)
-# @click.option('--max-frames', '-m', default=12, type=int)
 def main(**kwargs):
-    for x in dataset(2):
-        import alog
+    for x in dataset(1, **kwargs):
         alog.info(x)
-        sys.exit(0)
 
 
 if __name__ == '__main__':
