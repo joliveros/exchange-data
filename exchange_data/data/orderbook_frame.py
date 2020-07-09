0a3838a1cdc93bbceabebfbb1515105f4a944ff9
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-07-09 17:20:42 -0500

    wip
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/data/orderbook_frame.py
+++ exchange_data/data/orderbook_frame.py
@@ -23,14 +23,17 @@ pd.options.plotting.backend = 'plotly'
 
 
 class OrderBookFrame(MeasurementFrame, FrameNormalizer):
+    volume_max = 0.0
+
     def __init__(
         self,
         depth,
         database_name,
+        max_volume_quantile,
         interval,
         sequence_length,
+        window_size,
         symbol,
-        volume_max,
         volatility_intervals=False,
         **kwargs
     ):
@@ -40,24 +43,60 @@ class OrderBookFrame(MeasurementFrame, FrameNormalizer):
             interval=interval,
             symbol=symbol,
             **kwargs)
+        self.max_volume_quantile = max_volume_quantile
+        self.window_size = window_size
         self.depth = depth
         self.symbol = symbol
         self.volatility_intervals = volatility_intervals
-        self.volume_max = volume_max
         self.sequence_length = sequence_length
 
     def frame(self):
         frames = []
+
         for interval in self.intervals:
             self.start_date = interval[0]
             self.end_date = interval[1]
-            df = self.load_frames()
+            frames.append(self.load_frames())
 
-            frames.append(df)
         df = pd.concat(frames)
-        alog.info(f'### observed_max {self.observed_max} ###')
+
         df = df.set_index('time')
         df = df.sort_index()
+        df.dropna(how='any', inplace=True)
+
+        alog.info(df)
+
+        imgs = []
+
+        orderbook_img = df.orderbook_img.to_numpy().tolist()
+        df.drop(['orderbook_img'], axis=1)
+        orderbook_img = np.asarray(orderbook_img)
+
+        alog.info(orderbook_img.shape)
+
+        orderbook_img = np.concatenate((
+            orderbook_img[:, :, 0],
+            orderbook_img[:, :, 1]),
+            axis=2
+        )
+
+        orderbook_img = np.sort(orderbook_img, axis=2)
+
+        alog.info(orderbook_img.shape)
+
+        orderbook_img = np.delete(orderbook_img, 0, axis=3)
+
+        self.quantile = np.quantile(orderbook_img, self.max_volume_quantile)
+
+        orderbook_img = orderbook_img / self.quantile
+
+        orderbook_img = np.clip(orderbook_img, a_min=0.0, a_max=1.0)
+
+        df['orderbook_img'] = [
+            orderbook_img[i] for i in range(0, orderbook_img.shape[0])
+        ]
+
+        alog.info(df)
 
         return df
 
@@ -77,8 +116,9 @@ class OrderBookFrame(MeasurementFrame, FrameNormalizer):
 
     def load_frames(self):
         frames = []
+
         self.start_date = self.start_date - timedelta(seconds=timeparse(
-            self.group_by) * self.sequence_length)
+            self.group_by) * self.sequence_length * 2)
 
         levels = OrderBookLevelStreamer(
             database_name=self.database_name,
@@ -87,15 +127,15 @@ class OrderBookFrame(MeasurementFrame, FrameNormalizer):
             groupby=self.group_by,
             start_date=self.start_date,
             symbol=self.symbol,
-            window_size='15m'
+            window_size=self.window_size
         )
 
         orderbook_imgs = deque(maxlen=self.sequence_length)
 
         for timestamp, best_ask, best_bid, orderbook_img in levels:
-            if orderbook_img:
+
+            if orderbook_img is not None:
                 orderbook_img = np.asarray(json.loads(orderbook_img))
-                orderbook_img = self.normalize_frame(orderbook_img, self.volume_max)
                 orderbook_imgs.append(orderbook_img)
 
                 if len(orderbook_imgs) == self.sequence_length:
@@ -103,28 +143,30 @@ class OrderBookFrame(MeasurementFrame, FrameNormalizer):
                         time=timestamp,
                         best_ask=best_ask,
                         best_bid=best_bid,
-                        orderbook_img=np.array(list(orderbook_imgs.copy()))
+                        orderbook_img=np.asarray(list(orderbook_imgs.copy()))
                     )
 
                     frames.append(frame)
 
-        return DataFrame(frames)
+        df = DataFrame(frames)
+
+        return df
 
 
 @click.command()
 @click.option('--database_name', '-d', default='binance', type=str)
 @click.option('--depth', default=40, type=int)
 @click.option('--group-by', '-g', default='1m', type=str)
-@click.option('--interval', '-i', default='3d', type=str)
+@click.option('--interval', '-i', default='4h', type=str)
 @click.option('--plot', '-p', is_flag=True)
 @click.option('--sequence-length', '-l', default=48, type=int)
 @click.option('--tick', is_flag=True)
+@click.option('--max-volume-quantile', '-m', default=0.99, type=float)
 @click.option('--volatility-intervals', '-v', is_flag=True)
-@click.option('--volume-max', default=12000, type=float)
-@click.option('--window-size', '-w', default='15m', type=str)
+@click.option('--window-size', '-w', default='3m', type=str)
 @click.argument('symbol', type=str)
 def main(**kwargs):
-    frame = OrderBookFrame(**kwargs)
+    frame = OrderBookFrame(**kwargs).frame()
 
 
 if __name__ == '__main__':
