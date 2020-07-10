e925012abfb28613458ce851c122e7cf4edc8e23
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-07-10 11:42:55 -0500

    wip
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/models/resnet/expected_position.py
+++ exchange_data/models/resnet/expected_position.py
@@ -3,45 +3,11 @@ import numpy as np
 import pandas as pd
 
 
-def expected_position_frame(df, expected_position_length, take_ratio=1.0,
-                            pos_change_min=0.6, pos_change_max=0.6):
+def expected_position_frame(df, take_ratio=1.0, **kwargs):
     df = df.copy()
     df.dropna(how='any', inplace=True)
     df = df.sort_index()
     df = df.reset_index(drop=False)
-
-    change_df = df['best_ask'] - df['best_ask'].shift(1)
-    change_df.dropna(how='any', inplace=True)
-    change = change_df.to_numpy()
-    pos_change = change[change > 0.0]
-    min_change = np.quantile(pos_change, pos_change_min)
-    max_change = np.quantile(pos_change, pos_change_max)
-
-    change = df['best_ask'] - df['best_ask'].shift(1)
-
-    df['expected_position'] = np.where((change >= min_change) & (change <=
-                                       max_change), 1, 0)
-
-    if expected_position_length > 1:
-        for i in range(0, len(df)):
-            expected_position = df.loc[i, 'expected_position'] == 1
-
-            if expected_position:
-                for x in range(0, expected_position_length):
-                    y = i - x
-                    if y >= 0:
-                        df.loc[y, 'expected_position'] = 1
-
-    df.astype({'expected_position': 'int32'})
-    df.dropna(how='any', inplace=True)
-
-    # for i in range(0, len(df)):
-    #     row = df.loc[i]
-    #     alog.info(row)
-    #
-    # alog.info(df)
-    #
-    # raise Exception()
 
     flat_df = df[df['expected_position'] == 0]
     long_df = df[df['expected_position'] == 1]
