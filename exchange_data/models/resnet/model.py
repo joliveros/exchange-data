488ad866d5027a98439b06680c0572ed026ffe54
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-07-12 02:41:36 -0500

    generate network
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/models/resnet/model.py
+++ exchange_data/models/resnet/model.py
@@ -1,115 +1,86 @@
 #!/usr/bin/env python
 
-from pathlib import Path
-from pytimeparse.timeparse import timeparse
-from tensorflow_estimator.python.estimator.keras import model_to_estimator
-from tensorflow_estimator.python.estimator.run_config import RunConfig
-from tensorflow_estimator.python.estimator.training import TrainSpec, EvalSpec, train_and_evaluate
-
 import alog
 import click
-import logging
-import os
-import shutil
 import tensorflow as tf
 
+Conv2D = tf.keras.layers.Conv2D
+ConvLSTM2D = tf.keras.layers.ConvLSTM2D
 Dense = tf.keras.layers.Dense
 Dropout = tf.keras.layers.Dropout
 Flatten = tf.keras.layers.Flatten
-GlobalAveragePooling2D = tf.keras.layers.GlobalAveragePooling2D
 GlobalAveragePooling1D = tf.keras.layers.GlobalAveragePooling1D
+GlobalAveragePooling2D = tf.keras.layers.GlobalAveragePooling2D
 Input = tf.keras.Input
+LeakyReLU = tf.keras.layers.LeakyReLU
 LSTM = tf.keras.layers.LSTM
-ConvLSTM2D = tf.keras.layers.ConvLSTM2D
+MaxPooling2D = tf.keras.layers.MaxPooling2D
 Reshape = tf.keras.layers.Reshape
 Sequential = tf.keras.models.Sequential
 TimeDistributed = tf.keras.layers.TimeDistributed
-Conv2D = tf.keras.layers.Conv2D
-LeakyReLU = tf.keras.layers.LeakyReLU
-MaxPooling2D = tf.keras.layers.MaxPooling2D
+
 
 def Model(
     levels,
+    inception_units,
+    filters,
+    lstm_units,
     sequence_length,
-    batch_size=12,
     learning_rate=5e-5,
     num_categories=2,
-    include_last=True,
     **kwargs
 ):
     input_shape = (sequence_length, levels * 2, 1)
-
     alog.info(input_shape)
 
-    filters = 32
+    filters = filters * 16
 
     inputs = Input(shape=input_shape)
 
     alog.info(inputs.shape)
 
-    # build the convolutional block
-    conv_first1 = Conv2D(filters, (1, 2), strides=(1, 2))(inputs)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
+    filter_height = [2, ]
+    last_layer_filter_height = 0
+    last_conv = inputs
 
-    alog.info(conv_first1.shape)
+    while last_layer_filter_height != 1:
+        for i in range(0, len(filter_height)):
+            if last_layer_filter_height != 1:
+                f = filter_height[i]
+                if (last_layer_filter_height - f) < 0:
+                    f = f - 1
 
-    conv_first1 = Conv2D(filters, (1, 2), strides=(1, 2))(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
+                last_conv = conv_block(filters, last_conv, f)
+                last_layer_filter_height = last_conv.shape[2]
+                alog.info(last_conv.shape)
 
-    alog.info(conv_first1.shape)
+                if f not in filter_height:
+                    break
 
-    conv_first1 = Conv2D(filters, (1, 7))(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
+    conv = last_conv
 
-    alog.info(conv_first1.shape)
+    inception_units = inception_units * 16
+    lstm_units = lstm_units * 16
 
-    conv_first1 = Conv2D(filters, (1, 8))(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-
-    alog.info(conv_first1.shape)
-
-    conv_first1 = Conv2D(filters, (1, 7))(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-    conv_first1 = Conv2D(filters, (4, 1), padding='same')(conv_first1)
-    conv_first1 = LeakyReLU(alpha=0.01)(conv_first1)
-
-    alog.info(conv_first1.shape)
+    alog.info(conv.shape)
     # build the inception module
-    convsecond_1 = Conv2D(64, (1, 1), padding='same')(conv_first1)
+    convsecond_1 = Conv2D(inception_units, (1, 1), padding='same')(conv)
     convsecond_1 = LeakyReLU(alpha=0.01)(convsecond_1)
-    convsecond_1 = Conv2D(64, (3, 1), padding='same')(convsecond_1)
+    convsecond_1 = Conv2D(inception_units, (3, 1), padding='same')(convsecond_1)
     convsecond_1 = LeakyReLU(alpha=0.01)(convsecond_1)
 
     alog.info(convsecond_1.shape)
 
-    convsecond_2 = Conv2D(64, (1, 1), padding='same')(conv_first1)
+    convsecond_2 = Conv2D(inception_units, (1, 1), padding='same')(conv)
     convsecond_2 = LeakyReLU(alpha=0.01)(convsecond_2)
-    convsecond_2 = Conv2D(64, (5, 1), padding='same')(convsecond_2)
+    convsecond_2 = Conv2D(inception_units, (5, 1), padding='same')(convsecond_2)
     convsecond_2 = LeakyReLU(alpha=0.01)(convsecond_2)
 
     alog.info(convsecond_2.shape)
 
     convsecond_3 = MaxPooling2D((3, 1), strides=(1, 1), padding='same')(
-        conv_first1)
-    convsecond_3 = Conv2D(64, (1, 1), padding='same')(convsecond_3)
+        conv)
+    convsecond_3 = Conv2D(inception_units, (1, 1), padding='same')(convsecond_3)
     convsecond_3 = LeakyReLU(alpha=0.01)(convsecond_3)
 
     alog.info(convsecond_3.shape)
@@ -125,7 +96,7 @@ def Model(
         convsecond_output)
 
     # build the last LSTM layer
-    lstm_out = LSTM(64, return_sequences=False, stateful=False)(conv_reshape)
+    lstm_out = LSTM(lstm_units, return_sequences=False, stateful=False)(conv_reshape)
 
     alog.info(lstm_out.shape)
     alog.info(num_categories)
@@ -143,156 +114,23 @@ def Model(
     return model
 
 
-class ModelTrainer(object):
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-
-    def done(self):
-        self.publish('resnet_trainer_done', '')
-
-    def run(self):
-        return self._run(**self.kwargs)
-
-    def _run(
-            self,
-            train_df,
-            eval_df,
-            batch_size,
-            clear,
-            directory,
-            epochs,
-            export_model,
-            learning_rate,
-            levels,
-            sequence_length,
-            seed=None,
-            symbol=None,
-            **kwargs
-        ):
-
-        model_dir = f'{Path.home()}/.exchange-data/models/resnet/{directory}'
-
-        if clear:
-            try:
-                shutil.rmtree(model_dir)
-            except Exception:
-                pass
-
-        model = Model(
-            levels=levels,
-            sequence_length=sequence_length,
-            batch_size=batch_size,
-            learning_rate=learning_rate,
-        )
-
-        model.summary()
-
-        run_config = RunConfig(
-            save_checkpoints_secs=timeparse('4h'),
-            tf_random_seed=seed
-        )
-
-        resnet_estimator = model_to_estimator(
-            keras_model=model,
-            model_dir=model_dir,
-            checkpoint_format='saver',
-            config=run_config,
-        )
-
-        if symbol is None:
-            symbol = directory
-
-        def iterate_rows(_df):
-            def _rows():
-                for i in range(0, len(_df)):
-                    row = _df.iloc[i]
-                    yield row['orderbook_img'], row['expected_position']
-
-            return _rows
-
-        def train_ds():
-            return tf.data.Dataset.from_generator(
-                iterate_rows(train_df),
-                (tf.float32, tf.int32),
-                (tf.TensorShape([sequence_length, levels * 2, 1]),
-                 tf.TensorShape([]))
-            ) \
-                .cache() \
-                .batch(batch_size).repeat(epochs)
-
-        def eval_ds():
-            return tf.data.Dataset.from_generator(
-                iterate_rows(eval_df),
-                (tf.float32, tf.int32),
-                (tf.TensorShape([sequence_length, levels * 2, 1]),
-                 tf.TensorShape([]))
-            ) \
-                .batch(1)
-
-
-        # for frame, expected_position in tfds.as_numpy(train_ds()):
-        #     alog.info(frame)
-        #     alog.info(frame.shape)
-        #     alog.info(expected_position)
-        #     raise Exception()
-
-        train_spec = TrainSpec(
-            input_fn=train_ds,
-        )
-
-        eval_spec = EvalSpec(
-            input_fn=eval_ds,
-            start_delay_secs=timeparse('4h'),
-            steps=timeparse('16m'),
-            throttle_secs=timeparse('4h')
-        )
-
-        result = train_and_evaluate(resnet_estimator, train_spec, eval_spec)[0]
-
-        alog.info(result)
-
-        def serving_input_receiver_fn():
-            inputs = {
-              'input_1': tf.compat.v1.placeholder(
-                  tf.float32, [1, sequence_length, levels * 2, 1]
-              ),
-            }
-            return tf.estimator.export.ServingInputReceiver(inputs, inputs)
-
-        if export_model:
-            export_dir = f'{Path.home()}/.exchange-data/models/' \
-                         f'{symbol}_export'
-
-            try:
-                os.mkdir(export_dir)
-            except:
-                pass
-
-            exp_path = resnet_estimator.export_saved_model(export_dir,
-                                                serving_input_receiver_fn)
-
-            result['exported_model_path'] = exp_path.decode()
-
-        return result
+def conv_block(filters, last_conv, filter_height=2):
+    conv = Conv2D(filters, (1, filter_height), strides=(1, filter_height))(
+        last_conv)
+    conv = LeakyReLU(alpha=0.01)(conv)
+    conv = Conv2D(filters, (4, 1), padding='same')(conv)
+    conv = LeakyReLU(alpha=0.01)(conv)
+    conv = Conv2D(filters, (4, 1), padding='same')(conv)
+    last_conv = LeakyReLU(alpha=0.01)(conv)
+    return last_conv
 
 
 @click.command()
 @click.option('--batch-size', '-b', type=int, default=1)
 @click.option('--levels', type=int, default=40)
 @click.option('--sequence-length', type=int, default=48)
-@click.option('--take', type=int, default=1000)
-@click.option('--take-ratio', type=float, default=0.5)
-@click.option('--epochs', type=int, default=500)
-@click.option('--directory', type=str, default='default')
-@click.option('--learning-rate', '-l', type=float, default=1e-5)
-@click.option('--seed', type=int, default=6*6*6)
-@click.option('--clear', '-c', is_flag=True)
-@click.option('--export-model', is_flag=True)
 def main(**kwargs):
-    logging.getLogger('tensorflow').setLevel(logging.INFO)
-    trainer = ModelTrainer(**kwargs)
-    trainer.run()
-
+    Model(**kwargs)
 
 if __name__ == '__main__':
     main()
---
+++ exchange_data/models/resnet/model_trainer.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from pytimeparse.timeparse import timeparse
+from tensorflow_estimator.python.estimator.keras import model_to_estimator
+from tensorflow_estimator.python.estimator.run_config import RunConfig
+from tensorflow_estimator.python.estimator.training import TrainSpec, EvalSpec, train_and_evaluate
+
+import alog
+import click
+import logging
+import os
+import shutil
+import tensorflow as tf
+
+from exchange_data.models.resnet.model import Model
+
+
+class ModelTrainer(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def done(self):
+        self.publish('resnet_trainer_done', '')
+
+    def run(self):
+        return self._run(**self.kwargs)
+
+    def _run(
+            self,
+            train_df,
+            eval_df,
+            batch_size,
+            clear,
+            directory,
+            epochs,
+            export_model,
+            learning_rate,
+            levels,
+            sequence_length,
+            seed=None,
+            symbol=None,
+            **kwargs
+        ):
+
+        model_dir = f'{Path.home()}/.exchange-data/models/resnet/{directory}'
+
+        if clear:
+            try:
+                shutil.rmtree(model_dir)
+            except Exception:
+                pass
+
+        model = Model(
+            levels=levels,
+            sequence_length=sequence_length,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            **kwargs
+        )
+
+        model.summary()
+
+        run_config = RunConfig(
+            save_checkpoints_secs=timeparse('4h'),
+            tf_random_seed=seed
+        )
+
+        resnet_estimator = model_to_estimator(
+            keras_model=model,
+            model_dir=model_dir,
+            checkpoint_format='saver',
+            config=run_config,
+        )
+
+        if symbol is None:
+            symbol = directory
+
+        def iterate_rows(_df):
+            def _rows():
+                for i in range(0, len(_df)):
+                    row = _df.iloc[i]
+                    yield row['orderbook_img'], row['expected_position']
+
+            return _rows
+
+        def train_ds():
+            return tf.data.Dataset.from_generator(
+                iterate_rows(train_df),
+                (tf.float32, tf.int32),
+                (tf.TensorShape([sequence_length, levels * 2, 1]),
+                 tf.TensorShape([]))
+            ) \
+                .cache() \
+                .batch(batch_size).repeat(epochs)
+
+        def eval_ds():
+            return tf.data.Dataset.from_generator(
+                iterate_rows(eval_df),
+                (tf.float32, tf.int32),
+                (tf.TensorShape([sequence_length, levels * 2, 1]),
+                 tf.TensorShape([]))
+            ) \
+                .batch(1)
+
+
+        # for frame, expected_position in tfds.as_numpy(train_ds()):
+        #     alog.info(frame)
+        #     alog.info(frame.shape)
+        #     alog.info(expected_position)
+        #     raise Exception()
+
+        train_spec = TrainSpec(
+            input_fn=train_ds,
+        )
+
+        eval_spec = EvalSpec(
+            input_fn=eval_ds,
+            start_delay_secs=timeparse('4h'),
+            steps=timeparse('16m'),
+            throttle_secs=timeparse('4h')
+        )
+
+        result = train_and_evaluate(resnet_estimator, train_spec, eval_spec)[0]
+
+        alog.info(result)
+
+        def serving_input_receiver_fn():
+            inputs = {
+              'input_1': tf.compat.v1.placeholder(
+                  tf.float32, [1, sequence_length, levels * 2, 1]
+              ),
+            }
+            return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+
+        if export_model:
+            export_dir = f'{Path.home()}/.exchange-data/models/' \
+                         f'{symbol}_export'
+
+            try:
+                os.mkdir(export_dir)
+            except:
+                pass
+
+            exp_path = resnet_estimator.export_saved_model(export_dir,
+                                                serving_input_receiver_fn)
+
+            result['exported_model_path'] = exp_path.decode()
+
+        return result
+
+
+@click.command()
+@click.option('--batch-size', '-b', type=int, default=1)
+@click.option('--levels', type=int, default=40)
+@click.option('--sequence-length', type=int, default=48)
+@click.option('--take', type=int, default=1000)
+@click.option('--take-ratio', type=float, default=0.5)
+@click.option('--epochs', type=int, default=500)
+@click.option('--directory', type=str, default='default')
+@click.option('--learning-rate', '-l', type=float, default=1e-5)
+@click.option('--seed', type=int, default=6*6*6)
+@click.option('--clear', '-c', is_flag=True)
+@click.option('--export-model', is_flag=True)
+def main(**kwargs):
+    logging.getLogger('tensorflow').setLevel(logging.INFO)
+    trainer = ModelTrainer(**kwargs)
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
