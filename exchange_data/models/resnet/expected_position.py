355e33a20f6d96922ba369df36fe52ffa3e3d0d8
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-07-16 12:07:01 -0500

    resnet for timeseries
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/models/resnet/expected_position.py
+++ exchange_data/models/resnet/expected_position.py
@@ -9,31 +9,32 @@ def expected_position_frame(df, take_ratio=1.0, **kwargs):
     df = df.sort_index()
     df = df.reset_index(drop=False)
 
-    flat_df = df[df['expected_position'] == 0]
+    # flat_df = df[df['expected_position'] == 0]
     long_df = df[df['expected_position'] == 1]
-    negative_df = df[df['large_negative_change'] == 1]
 
-    max_len = max([len(flat_df), len(long_df), len(negative_df)])
+    flat_df = df[df['large_negative_change'] == 1]
+    # flat_df = pd.concat([flat_df, negative_df.sample(frac=take_ratio, replace=True)])
 
-    flat_df = resample_to_len(flat_df, max_len)
+    flat_df = resample_to_len(flat_df, int(len(long_df) * take_ratio))
+    long_df = long_df.sample(frac=take_ratio, replace=True)
 
-    long_df = resample_to_len(long_df, max_len)
+    # if take_ratio != 1.0:
+    #     long_df = long_df.sample(frac=take_ratio, random_state=0, replace=True)
 
-    negative_df = resample_to_len(negative_df, max_len)
-
-    if take_ratio != 1.0:
-        long_df = long_df.sample(frac=take_ratio, random_state=0, replace=True)
-
-    # flat_df = resample_to_len(short_df, max_len)
-
-    df = pd.concat((long_df, flat_df, negative_df))
+    df = pd.concat((long_df, flat_df))
 
     return df
 
 
 def resample_to_len(df, max_len):
-    if len(df) < max_len:
-        df = df.sample(frac=max_len / len(df),
-                                 random_state=0,
-                                 replace=True)
+    len_df = len(df)
+    if len_df > max_len:
+        frac = max_len / len_df
+    else:
+        frac = len_df / max_len
+
+    df = df.sample(frac=frac,
+                   random_state=0,
+                   replace=True)
+
     return df
