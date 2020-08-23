6e5bd26b181bf7df3ebba77da444c63a4a85ff9d
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-08-22 21:46:40 -0500

    trade emitter
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/data/measurement_frame.py
+++ exchange_data/data/measurement_frame.py
@@ -84,8 +84,9 @@ class MeasurementFrame(MeasurementMeta):
         frames = []
 
         for data in self.query(query).get_points(self.name):
+            alog.info(alog.pformat(data))
             timestamp = self.parse_db_timestamp(data['time'])
-            data = data['data_data'] or {}
+            data = data.get('data_data', None) or {}
 
             if type(data) is str:
                 data = pd.read_json(data)
@@ -104,7 +105,6 @@ class MeasurementFrame(MeasurementMeta):
 
                 frames.append(data)
 
-
         df = DataFrame.from_dict(frames)
 
         df['time'] = pd.to_datetime(df['time'])
@@ -114,6 +114,35 @@ class MeasurementFrame(MeasurementMeta):
 
         return df
 
+    def frame_all_keys(self):
+        query = f'SELECT first("short_period") AS short_period, ' \
+                f'first("long_period") as long_period, first("group_by_min") ' \
+                f'as group_by_min, first("symbol") as symbol, first("value") ' \
+                f'as value FROM' \
+                f' {self.name} WHERE time >=' \
+                f' {self.formatted_start_date} AND ' \
+                f'time <= {self.formatted_end_date} GROUP BY time(' \
+                f'{self.group_by})'
+
+        alog.info(query)
+
+        frames = []
+
+        for data in self.query(query).get_points(self.name):
+            timestamp = self.parse_db_timestamp(data['time'])
+            data['time'] = timestamp
+
+            frames.append(data)
+
+        df = DataFrame.from_dict(frames)
+
+        df['time'] = pd.to_datetime(df['time'])
+
+        df.set_index('time', inplace=True)
+        df.sort_index(inplace=True)
+        df.dropna(subset=['value'], inplace=True)
+
+        return df
 
     def append(self, data: dict, timestamp: datetime = None):
         if timestamp is None:
@@ -124,7 +153,5 @@ class MeasurementFrame(MeasurementMeta):
             fields=data,
             time=timestamp
         )
-
-        alog.info(alog.pformat(m))
 
         self.write_points([m.__dict__])
---
+++ exchange_data/emitters/binance/_trade_executor.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+
+from decimal import Decimal, getcontext
+from binance.client import Client
+from binance.enums import SIDE_BUY, SIDE_SELL, ORDER_TYPE_MARKET
+from cached_property import cached_property, cached_property_with_ttl
+from pytimeparse.timeparse import timeparse
+from redis_collections import Dict
+
+from exchange_data import settings
+from exchange_data.data.measurement_frame import MeasurementFrame
+from exchange_data.emitters import Messenger, binance
+from exchange_data.ta_model.backtest import MacdParamFrame, BackTest
+
+import alog
+import binance
+import click
+import signal
+import sys
+
+from exchange_data.trading import Positions
+
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+class MacdParams(object):
+    def __init__(
+        self,
+        group_by_min=0,
+        long_period=0,
+        short_period=0,
+        symbol=None,
+        **kwargs
+    ):
+        self.group_by_min = group_by_min
+        self.long_period = long_period
+        self.short_period = short_period
+        self.symbol = symbol
+
+    def __repr__(self):
+        return alog.pformat(vars(self))
+
+
+class TradeExecutor(MeasurementFrame, Messenger):
+    measurements = []
+    _last_params = None
+    current_position = Positions.Flat
+    symbol = None
+
+    def __init__(self, base_asset, fee=0.0075, **kwargs):
+        super().__init__(
+            **kwargs
+        )
+        self.fee = fee
+        self.base_asset = base_asset
+        self.asset = None
+        self.on('30s', self.trade)
+
+        self.client = Client(
+            api_key=settings.BINANCE_API_KEY,
+            api_secret=settings.BINANCE_API_SECRET
+        )
+
+    @property
+    def last_params(self):
+        return MacdParams(**Dict(key=f'{self.symbol}_params',
+                                 redis=self.redis_client))
+
+    @last_params.setter
+    def last_params(self, value: MacdParams):
+        self.symbol = value.symbol
+
+        self._last_params = Dict(data=vars(value), key=f'{self.symbol}_params',
+                                 redis=self.redis_client)
+
+    @cached_property_with_ttl(ttl=timeparse('10m'))
+    def exchange_info(self):
+        return self.client.get_exchange_info()
+
+    @property
+    def symbol_info(self):
+        return [info for info in self.exchange_info['symbols'] if
+                   info['symbol'] == self.symbol][0]
+
+    @property
+    def asset_name(self):
+        return self.symbol.replace(self.base_asset, '')
+
+    @property
+    def lot_size(self):
+        return [filter for filter in self.symbol_info['filters'] if filter[
+            'filterType'] == 'LOT_SIZE'][0]
+
+    @property
+    def price_filter(self):
+        return [filter for filter in self.symbol_info['filters'] if
+                        filter[
+            'filterType'] == 'PRICE_FILTER'][0]
+
+    @property
+    def step_size(self):
+        return float(self.lot_size['stepSize'])
+
+    @property
+    def precision(self):
+        return self.symbol_info['quoteAssetPrecision']
+
+    @property
+    def bid_price(self):
+        getcontext().prec = self.precision
+
+        ticker = self.client.get_orderbook_ticker(symbol=self.symbol)
+
+        price = int((float(ticker['bidPrice'])) /
+                   self.tick_size) * self.tick_size
+
+        price = Decimal(price)
+
+        return round(price, self.precision)
+
+    @property
+    def tick_size(self):
+        return float(self.price_filter['tickSize'])
+
+    @property
+    def quantity(self):
+        getcontext().prec = self.precision
+
+        quantity = self.balance / (float(self.bid_price) * (1 + self.fee))
+
+        quantity = float(int(quantity / self.step_size) * self.step_size)
+
+        quantity = Decimal(quantity)
+
+        quantity = truncate(quantity, self.precision)
+
+        return quantity
+
+    @property
+    def min_quantity(self):
+        getcontext().prec = self.precision
+
+        quantity = 0.15 / (float(self.bid_price))
+
+        quantity = float(int(quantity / self.step_size) * self.step_size)
+
+        quantity = Decimal(quantity)
+
+        quantity = truncate(quantity, self.precision)
+
+        return quantity
+
+    @property
+    def asset_quantity(self):
+        getcontext().prec = self.precision
+
+        quantity = self.client.get_asset_balance(self.asset_name)['free']
+
+        quantity = Decimal(quantity)
+
+        return quantity
+
+    @cached_property_with_ttl(ttl=1)
+    def order(self):
+        orders = self.orders
+
+        if len(self.orders) \
+            > 0:
+            return orders[0]
+        else:
+            return None
+
+    def trade(self, timestamp=None):
+        if not self.last_params.symbol:
+            self.last_params = self.best_params()
+
+        alog.info(self.position)
+
+        alog.info(self.orders)
+
+        if len(self.orders) == 0 and self.quantity > 0  and self.position == \
+            Positions.Long and self.asset_quantity < self.min_quantity:
+            # add limit orders
+
+            alog.info(self.quantity)
+            alog.info(self.bid_price)
+
+            self.long()
+
+        if self.order:
+            price = Decimal(self.order['price'])
+            if price != self.bid_price:
+                params = dict(symbol=self.order['symbol'],
+                             orderId=self.order['orderId'])
+
+                alog.info(alog.pformat(params))
+
+                self.client.cancel_order(**params)
+
+                if self.position == Positions.Long:
+                    self.long()
+
+        if self.position == Positions.Flat and \
+            self.asset_quantity > self.tick_size:
+            params = dict(
+                symbol=self.symbol,
+                side=SIDE_SELL,
+                type=ORDER_TYPE_MARKET,
+                quantity=self.asset_quantity,
+            )
+
+            alog.info(alog.pformat(params))
+
+            self.client.create_order(**params)
+
+            self.last_params = self.best_params()
+
+    def long(self):
+
+        alog.info(self.min_quantity)
+
+        response = self.client.create_order(
+            symbol=self.symbol,
+            side=SIDE_BUY,
+            type=binance.enums.ORDER_TYPE_MARKET,
+            quantity=self.quantity,
+        )
+        alog.info(alog.pformat(response))
+
+    @cached_property_with_ttl(ttl=4)
+    def position(self) -> Positions:
+        df = BackTest(symbol=self.last_params.symbol,
+                      database_name=self.database_name,
+                      interval='1d').label_position(**vars(self.last_params))
+        return df.iloc[-1]['position']
+
+    def best_params(self) -> MacdParams:
+        params_df = MacdParamFrame(database_name=self.database_name,
+                                   interval=self.interval_str)\
+            .frame_all_keys()
+
+        params = MacdParams(
+            **params_df.loc[params_df['value'].idxmax()].to_dict()
+        )
+
+        return params
+
+    @property
+    def account_data(self):
+
+        data = dict(
+            account_status=self.client.get_account_status(),
+            balance=self.client.get_asset_balance(self.base_asset),
+            account_assets=self.client.get_sub_account_assets()
+            # trades=self.client.get_my_trades(symbol=self.symbol)
+            # dust_log=self.client.get_dust_log(),
+            # trade_fee=self.client.get_trade_fee(symbol=self.symbol)
+        )
+
+        return data
+
+    @property
+    def balance(self):
+        return float(self.account_data['balance']['free'])
+
+    @property
+    def orders(self):
+        orders = self.client.get_all_orders(symbol=self.symbol)
+
+        return [order for order in orders if order['status'] == 'NEW']
+
+    def start(self):
+        self.sub(['30s'])
+
+    def stop(self):
+        sys.exit(0)
+
+
+@click.command()
+@click.option('--database-name', '-d', default='binance', type=str)
+@click.option('--base-asset', '-b', default='BNB', type=str)
+@click.option('--interval', '-i', default='6h', type=str)
+def main(**kwargs):
+    emitter = TradeExecutor(**kwargs)
+    emitter.start()
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, lambda: exit(0))
+    signal.signal(signal.SIGTERM, lambda: exit(0))
+    main()
