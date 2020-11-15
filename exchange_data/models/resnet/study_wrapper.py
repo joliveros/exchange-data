2d0faea382cb7048182a7dfcee768e8c7e8c8a86
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-11-15 15:51:06 -0600

    add min-capital param
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

---
+++ exchange_data/models/resnet/study_wrapper.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import optuna
+from optuna import load_study
+
+
+class StudyWrapper(object):
+    study_db_path: Path = None
+
+    def __init__(self, symbol, **kwargs):
+        self.symbol = symbol
+        self.base_path = f'{Path.home()}/.exchange-data/models/'
+        self.study_db_path = f'{Path.home()}/.exchange-data/models/{self.symbol}.db'
+        self.study_db_path = Path(self.study_db_path)
+        db_conn_str = f'sqlite:///{self.study_db_path}'
+
+        if not self.study_db_path.exists():
+            self.create_study(db_conn_str)
+        else:
+            try:
+                self.study = load_study(study_name=self.symbol, storage=db_conn_str)
+            except KeyError as e:
+                self.create_study(db_conn_str)
+
+    def create_study(self, db_conn_str):
+        self.study = optuna.create_study(
+            study_name=self.symbol, direction='maximize',
+            storage=db_conn_str)
