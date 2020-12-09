71a55bd6cb345d283aace5017a18cb2a6daea8d2
Jose Oliveros <jose.oliveros.1983@gmail.com>
2020-12-08 20:42:08 -0600

    add another list
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/proxies.py
+++ exchange_data/proxies.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 from exchange_data import settings
 from exchange_data.emitters.binance._depth_emitter import ProxiedClient
@@ -18,70 +19,96 @@ cache = RedisCache(redis_client=Redis(host=settings.REDIS_HOST))
 
 
 def test_proxy(proxy):
-  proxies = dict(
-    http=proxy,
-    https=proxy
-  )
+    proxies = dict(
+        http=proxy,
+        https=proxy
+    )
 
-  try:
-    ProxiedClient(proxies)
-    alog.info(f'## good proxy {proxy} ##')
-    return (proxy, True)
-  except Exception as e:
-    alog.info(e)
-    return (proxy, False)
+    try:
+        ProxiedClient(proxies)
+        alog.info(f'## good proxy {proxy} ##')
+        return (proxy, True)
+    except Exception as e:
+        alog.info(e)
+        return (proxy, False)
 
 
 class Proxies(ProxiesBase):
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
+    def __init__(self, num_tests_per_proxy, min_ratio, **kwargs):
+        super().__init__(**kwargs)
+        self.num_tests_per_proxy = num_tests_per_proxy
+        self.min_ratio = min_ratio
 
-    self.proxies_queue.update(set(self.get_proxies()['hosts']))
-    self.proxies_queue.update(self.valid_proxies)
+        self.proxies_queue.update(set(self.get_proxies()['hosts']))
+        self.proxies_queue.update(self.get_free_proxy_list())
+        self.proxies_queue.update(self.valid_proxies)
 
-    tests = []
+        results_table = dict()
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-      while len(self.proxies_queue) > 0:
-        proxy = self.proxies_queue.pop()
-        tests.append(executor.submit(test_proxy, proxy))
+        for p in self.proxies_queue:
+            results_table[p] = 0
 
-    for test in tests:
-      test_result = test.result()
-      if test_result[-1]:
-        proxy = test_result[0]
-        self.valid_proxies.add(proxy)
-        alog.info(f'### valid {proxy} ###')
-      else:
-        if proxy in self.valid_proxies:
-          self.valid_proxies.remove(proxy)
-          alog.info(f'### remove {proxy} ###')
+        tests = []
 
-    alog.info(len(self.valid_proxies))
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            while len(self.proxies_queue) > 0:
+                proxy = self.proxies_queue.pop()
 
-  @property
-  def proxies_queue(self):
-    return Set(key='proxies_queue', redis=self.redis_client)
+                for t in range(0, self.num_tests_per_proxy):
+                    tests.append(executor.submit(test_proxy, proxy))
 
-  @staticmethod
-  @cache.cache(ttl=60 * 60)
-  def get_proxies():
-    with requests.session() as session:
-      res = session.get(
-        'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all')
+        for test in tests:
+            test_result = test.result()
+            if test_result[-1]:
+                proxy = test_result[0]
+                results_table[proxy] += 1
 
-      hosts = [host.strip() for host in res.text.split('\r\n')]
-      hosts = [host for host in hosts if len(host) > 0]
-      alog.info(hosts)
-      return dict(hosts=hosts)
+        results = [r[0] for r in results_table.items()
+                   if r[-1] / self.num_tests_per_proxy >= self.min_ratio]
+
+        for p in results:
+            self.valid_proxies.add(p)
+
+        alog.info(len(self.valid_proxies))
+
+    @property
+    def proxies_queue(self):
+        return Set(key='proxies_queue', redis=self.redis_client)
+
+    @staticmethod
+    @cache.cache(ttl=60 * 60)
+    def get_proxies():
+        with requests.session() as session:
+            res = session.get(
+                'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all')
+
+            hosts = [host.strip() for host in res.text.split('\r\n')]
+            hosts = [host for host in hosts if len(host) > 0]
+            alog.info(hosts)
+            return dict(hosts=hosts)
+
+    @staticmethod
+    @cache.cache(ttl=60 * 60)
+    def get_free_proxy_list_request():
+        url = 'https://free-proxy-list.net'
+        with requests.session() as session:
+            res = session.get(url)
+            return res.text
+
+    def get_free_proxy_list(self):
+        res = self.get_free_proxy_list_request()
+
+        return re.findall('\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}\:\d{1,6}', res)
 
 
 @click.command()
+@click.option('--num-tests-per-proxy', '-t', type=int, default=2)
+@click.option('--min-ratio', '-r', type=float, default=0.5)
 def main(**kwargs):
-  proxies = Proxies(**kwargs)
+    proxies = Proxies(**kwargs)
 
 
 if __name__ == '__main__':
-  signal.signal(signal.SIGINT, lambda *args: exit(0))
-  signal.signal(signal.SIGTERM, lambda *args: exit(0))
-  main()
+    signal.signal(signal.SIGINT, lambda *args: exit(0))
+    signal.signal(signal.SIGTERM, lambda *args: exit(0))
+    main()
