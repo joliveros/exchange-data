f42997e84417b2edbd3626ed42e0d3a38ee28b44
Jose Oliveros <jose.oliveros.1983@gmail.com>
2021-01-14 19:15:03 -0600

    automate tfserving config
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tfserving/config.py
+++ tfserving/config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import os
 from pathlib import Path
 from tensorflow_serving.sources.storage_path.file_system_storage_path_source_pb2 \
     import FileSystemStoragePathSourceConfig
@@ -9,35 +9,49 @@ import alog
 import click
 
 
+class EDModelServerConfig(object):
+    def __init__(self, symbol, path, base_path=None):
+        self.name = symbol
+
+        if 'best' in base_path:
+            self.name = f'best_{symbol}'
+
+        self.base_path = f'/models/{path}'
+
+        if base_path:
+            self.base_path = f'{base_path}/{path}'
+
+        self.model_platform = 'tensorflow'
+
+        self.model_version_policy.all.CopyFrom(
+            FileSystemStoragePathSourceConfig.ServableVersionPolicy.All())
+
+
 @click.command()
 def main(**kwargs):
-    models_dir = Path().home() / '.exchange-data/models/'
+    base_dir = '.exchange-data/models'
+    models_dir = Path().home() / '.exchange-data/models'
     exported_model_dirs = list(models_dir.rglob('*_export'))
-
-    exported_models = [
-        (p.name, p.name.split('_')[0]) for p in exported_model_dirs
-    ]
+    best_model_dir = '.exchange-data/best_exported_models'
 
     server_config: ModelServerConfig = ModelServerConfig()
 
-    for path, symbol in exported_models:
-        config = server_config.model_config_list.config.add()
-        config.name = symbol
-        config.base_path = f'/models/{path}'
-        config.model_platform = 'tensorflow'
+    for exp_dir in [base_dir, best_model_dir]:
+        exported_models = [
+            (p.name, p.name.split('_')[0]) for p in exported_model_dirs
+        ]
 
-        config.model_version_policy.all.CopyFrom(
-            FileSystemStoragePathSourceConfig.ServableVersionPolicy.All())
+        exported_models = [(path, symbol) for path, symbol in exported_models
+                            if len(os.listdir(Path().home()/ exp_dir / path)) > 0]
 
-        config = server_config.model_config_list.config.add()
-        config.name = f'best_{symbol}'
-        config.base_path = f'/best_exported_models/{symbol}'
-        config.model_platform = 'tensorflow'
+        for path, symbol in exported_models:
+            config = server_config.model_config_list.config.add()
 
-        config.model_version_policy.all.CopyFrom(
-            FileSystemStoragePathSourceConfig.ServableVersionPolicy.All())
+            EDModelServerConfig.__init__(config, symbol, path, exp_dir,
+                                         **kwargs)
 
-    config_file = Path.home() / Path('.exchange-data/models/models.config')
+    config_file = Path.home() / Path(
+        '.exchange-data/models/models.config')
 
     if config_file.exists():
         config_file.unlink()
