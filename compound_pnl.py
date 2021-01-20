d713383c57b5a12dc1a107d6485790b07aa5315b
Jose Oliveros <jose.oliveros.1983@gmail.com>
2021-01-20 12:44:04 -0600

    add max investment param
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- compound_pnl.py
+++ compound_pnl.py
@@ -5,7 +5,7 @@ from pytimeparse.timeparse import timeparse
 
 
 class CompoundPnl(object):
-    def __init__(self, capital, interval, trade_interval='10m',
+    def __init__(self, capital, max_investment, interval, trade_interval='10m',
                  rate=5/9650):
         self.capital = capital
         self.interval = timeparse(interval)
@@ -16,7 +16,15 @@ class CompoundPnl(object):
             # alog.info((1 + pnl_rate))
             # alog.info(self.capital * (1 + pnl_rate))
             # raise Exception()
-            self.capital = self.capital * (1 + rate)
+
+            capital = self.capital
+
+            if capital > max_investment:
+                capital = max_investment
+
+            self.capital -= capital
+
+            self.capital += capital * (1 + rate)
 
         alog.info(f'### {int(self.steps)} steps, capital: {self.capital}')
 
@@ -25,6 +33,7 @@ class CompoundPnl(object):
 @click.command()
 @click.option('--capital', '-c', type=float, default=1)
 @click.option('--rate', '-r', type=float, default=5/9650)
+@click.option('--max-investment', '-m', type=float, default=1400)
 @click.option('--interval', '-i', type=str, default='1d')
 @click.option('--trade-interval', '-t', type=str, default='30m')
 def main(**kwargs):
