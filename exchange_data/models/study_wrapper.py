692f8eb53afe6580b2834f787a4eef5c9727bf7e
Jose Oliveros <jose.oliveros.1983@gmail.com>
2023-01-20 20:15:13 -0600

    add params to wrapper
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- exchange_data/models/study_wrapper.py
+++ exchange_data/models/study_wrapper.py
@@ -1,12 +1,15 @@
+from cached_property import cached_property_with_ttl
+from optuna import Trial
+from optuna import load_study
+from optuna.storages import RDBStorage
 from os import environ
 from pathlib import Path
-from optuna import load_study
 from redis_collections import Dict
 
 import alog
 import optuna
 import sec
-from optuna.storages import RDBStorage
+import pandas as pd
 
 
 class StudyWrapper(object):
@@ -55,3 +58,68 @@ class StudyWrapper(object):
         self._best_study_params = \
             Dict(key=f'best_key_{self.symbol}', data=values,
                  redis=self.redis_client)
+    @property
+    def best_trial(self):
+        best_trial_id = self.best_trial_id
+
+        return Trial(trial_id=best_trial_id, study=self.study)
+
+    @cached_property_with_ttl(ttl=60)
+    def best_trial_params(self):
+        trial = self.best_trial
+
+        params = {**trial.params, **trial.user_attrs}
+
+        return params
+
+    @cached_property_with_ttl(ttl=60)
+    def best_tuned_trial_params(self):
+        trial = Trial(trial_id=self.best_tuned_trial_id, study=self.study)
+
+        params = {**trial.params, **trial.user_attrs}
+
+        return params
+
+    @cached_property_with_ttl(ttl=60)
+    def best_trial_id(self):
+        df = self.study.trials_dataframe()
+        df = df[df['value'] > 0.0]
+
+        pd.set_option('display.max_rows', len(df) + 1)
+
+        if not df.empty:
+            df = df[df['user_attrs_tuned'] == False]
+            df = df[df['state'] == 'COMPLETE']
+
+        if len(df) == 0:
+            raise NotEnoughTrialsException()
+
+        # row_id = df[['value']].idxmax()['value']
+
+        trial_id = int(df.iloc[-1]['number']) + 1
+
+        alog.info(df)
+
+        return trial_id
+
+    @cached_property_with_ttl(ttl=60)
+    def best_tuned_trial_id(self):
+        df = self.study.trials_dataframe()
+        df = df[df['value'] > 0.0]
+
+        pd.set_option('display.max_rows', len(df) + 1)
+
+        if not df.empty:
+            df = df[df['user_attrs_tuned'] == True]
+            df = df[df['state'] == 'COMPLETE']
+
+        if len(df) == 0:
+            raise NotEnoughTrialsException()
+
+        # row_id = df[['value']].idxmax()['value']
+
+        trial_id = int(df.iloc[-1]['number']) + 1
+
+        alog.info(df)
+
+        return trial_id
