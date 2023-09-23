ba06b671c63f10f6d82823e19e1eadc9df15df65
Jose Oliveros <jose.oliveros.1983@gmail.com>
2023-09-23 15:11:43 -0600

    show flat trades
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- tgym/envs/orderbook/_orderbook.py
+++ tgym/envs/orderbook/_orderbook.py
@@ -554,11 +554,8 @@ class OrderBookTradingEnv(Logging, Env):
         summary['position_history'] = \
             ''.join(self.position_history[-1 * self.max_summary:])
 
-        summary['trades'] = [trade for trade in self.trades[-1 * int(self.max_summary/3):]
-            if type(trade) != FlatTrade]
-
-        # summary['trades'] = [trade for trade in self.trades[-1 * self.max_summary:]
-        #     if type(trade) != FlatTrade and type(trade) != FlatRewardPnlDiffTrade]
+        summary['trades'] = [trade for trade in
+                             self.trades[-1 * int(self.max_summary/3):]]
 
         return summary
 
